bef8b50c5c5ad2029076d178b8816a524cf4b022
Xavier QUIÈVRE <xavier@synapse-interactive.fr>
2020-01-21 15:36:40 +0100

    Refacto

---
+++ intersection-observer.js
@@ -0,0 +1,63 @@
+class InViewportObserver {
+	visibleItems = [];
+	items = [];
+	current;
+
+	constructor(containerIdentifier, elementsIdentifier, threshold, callback) {
+		this.containerIdentifier = containerIdentifier;
+		this.elementsIdentifier = elementsIdentifier;
+		this.threshold = threshold;
+		this.callback = callback;
+	}
+
+	observe() {
+		this.observer = new IntersectionObserver(observables => {
+			for (const observable of observables) {
+				if (observable.intersectionRatio > this.threshold) {
+					if (this.getVisibleItemIndex(observable) === -1) {
+						this.visibleItems.push(observable);
+					}
+				} else {
+					const index = this.getVisibleItemIndex(observable);
+
+					if (index > -1) {
+						this.visibleItems.splice(index, 1);
+					}
+				}
+			}
+
+			this.visibleItems.sort(
+				(a, b) => Array.from(this.items).indexOf(a.target) > Array.from(this.items).indexOf(b.target) ? 1 : -1
+			);
+
+			if (this.visibleItems.length > 0) {
+				if (this.visibleItems[0].target.getAttribute('id') !== this.current) {
+					this.current = this.visibleItems[0].target.getAttribute('id');
+
+					this.callback(this.visibleItems[0]);
+				}
+			}
+		}, {
+			root: document.querySelector(this.containerIdentifier),
+			threshold: [this.threshold]
+		});
+
+		this.items = document.querySelectorAll(this.containerIdentifier + ' ' + this.elementsIdentifier);
+
+		this.items.forEach(item => this.observer.observe(item));
+	}
+
+	unobserve() {
+		this.items.forEach(item => this.observer.unobserve(item));
+	}
+
+	getVisibleItemIndex = function(observable) {
+		for (const i in this.visibleItems) {
+			if (observable.target.getAttribute('id') === this.visibleItems[i].target.getAttribute('id')) {
+				return i;
+			}
+		}
+
+		return -1;
+	}
+}
--- script.js
+++ script.js
@@ -1,70 +1,8 @@
-const threshold = 0.8;
+const inViewportObserver = new InViewportObserver(
+	'#container',
+	'section',
+	0.8,
+	observable => console.log(observable.target.getAttribute('id'))
+);
 
-let visibleItems = [];
-var current;
-
-let getVisibleItemIndex = function(observable) {
-	for (const i in visibleItems) {
-		if (observable.target.getAttribute('id') === visibleItems[i].target.getAttribute('id')) {
-			return i;
-		}
-	}
-
-	return -1;
-}
-
-const visibleItemsSort = function(a, b) {
-	const sections = document.querySelectorAll('section');
-
-	return Array.from(sections).indexOf(a.target) > Array.from(sections).indexOf(b.target) ? 1 : -1;
-}
-
-let observer = new IntersectionObserver(observables => {
-	for (const observable of observables) {
-		if (observable.intersectionRatio > threshold) {
-			if (getVisibleItemIndex(observable) === -1) {
-				visibleItems.push(observable);
-			}
-		} else {
-			const index = getVisibleItemIndex(observable);
-
-			if (index > -1) {
-				visibleItems.splice(index, 1);
-			}
-		}
-	}
-
-	visibleItems.sort(visibleItemsSort);
-
-	if (visibleItems.length > 0) {
-		if (visibleItems[0].target.getAttribute('id') !== current) {
-			current = visibleItems[0].target.getAttribute('id');
-
-			log(visibleItems[0].target.getAttribute('id'));
-		}
-	}
-
-	/*log('', true);
-	for (item of visibleItems) {
-		log(item.target.getAttribute('id'));
-	}*/
-}, {
-	root: document.querySelector('#container'),
-	threshold: [threshold]
-});
-
-let items = document.querySelectorAll('section');
-
-items.forEach(item => observer.observe(item));
-
-var log = function(text, clear) {
-	const element = document.createElement('div');
-
-	element.innerHTML = text;
-
-	if (clear) {
-		document.querySelector('#log').innerHTML = '';
-	}
-
-	document.querySelector('#log').appendChild(element);
-}
+inViewportObserver.observe();
