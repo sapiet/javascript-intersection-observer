f0143393104a739150cbab704a79c65e0554b9e4
Xavier QUIÈVRE <xavier@synapse-interactive.fr>
2020-01-21 15:38:52 +0100

    Spaces instead of indentations

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
 const inViewportObserver = new InViewportObserver(
-	'#container',
-	'section',
-	0.8,
-	observable => console.log(observable.target.getAttribute('id'))
+    '#container',
+    'section',
+    0.8,
+    observable => console.log(observable.target.getAttribute('id'))
 );
 
 inViewportObserver.observe();
